b3b4cdf751abe14cb2bc3b851c643a282b813bbd
prince_matin <matinx636@gmail.com>
2026-06-15 20:24:40 +0330

    Finalizing the calculator project with the highest level of code based on my current knowledge (The main and final codes are in the calc_final_version.py file)

--- calc_final_version.py
+++ calc_final_version.py
@@ -9,16 +9,16 @@ class Calculator:
 
     def Expression(self):
         TorF_val = False
-        expression = input('enter your expression : ')
+        expression = input('Expression : enter your expression : ')
         for i in expression:
             if ('a' <= i <= 'z') or ('A' <= i <= 'Z'):
-                print('Error: only num values are allowed!')
+                print('Expression : Error: only num values are allowed!')
                 break
             else :
                 TorF_val = True
         if TorF_val == True:
             ans = eval(expression)
-            print(f'this is your answer : {ans}')
+            print(f'Expression : this is your answer : {ans}')
             Calculator.last_ans = f'{expression} = {ans}'
 
 
@@ -26,21 +26,21 @@ class Calculator:
         TorF_val1 = False
         TorF_val2 = False
         while True:
-            mode = input(' 1 for percent / 2 for increase : ')
+            mode = input('Percent : 1 for percent / 2 for increase : ')
             if mode in ['1', '2']:
                 break
-            print('Invalid mode!')
+            print('Percent : Invalid mode!')
 
-        num1 = input('enter your number : ')
-        num2 = input('enter your percent : ')
+        num1 = input('Percent : enter your number : ')
+        num2 = input('Percent : enter your percent : ')
         for i in num1:
             if ('a' <= i <= 'z') or ('A' <= i <= 'Z'):
-                print('Error: only num values are allowed!')
+                print('Percent : Error: only num values are allowed!')
             else:
                 TorF_val1 = True
         for i in num2:
             if ('a' <= i <= 'z') or ('A' <= i <= 'Z'):
-                print('Error: only num values are allowed!')
+                print('Percent : Error: only num values are allowed!')
             else:
                 TorF_val2 = True
         if TorF_val1 and TorF_val2 == True:
@@ -49,20 +49,23 @@ class Calculator:
 
             if mode == '1':
                 ans = (num1 * num2) / 100
-                print(f'this is your answer : {ans}')
+                print(f'Percent : this is your answer : {ans}')
                 Calculator.last_ans = f'{num2}% of {num1} = {ans}'
 
             elif mode == '2':
                 ans = num1 + ((num1 * num2) / 100)
-                print(f'this is your answer : {ans}')
+                print(f'Percent : this is your answer : {ans}')
                 Calculator.last_ans = f'{num1} increased by {num2}% = {ans}'
         else:
-            print('Error: only num values are allowed!')
+            print('Percent : Error: only num values are allowed!')
 
 
     def last_ans_func(self):
-        print(f'this is your last calculating : {Calculator.last_ans}')
-
+        if Calculator.last_ans is None or Calculator.last_ans == "":
+            print('last_ans_func : you Calculator nothing!')
+        else :
+            print(f'last_ans_func : this is your last calculating : {Calculator.last_ans}')
+        
     
 
 
@@ -70,7 +73,8 @@ class Calculator:
 nums = []
 print('1 = Expression')
 print('2 = Percent')
-menu1 = input('enter what do you want : ')
+print('------------------------------------------------')
+menu1 = input('Calculator : enter what do you want : ')
 
 # add args and rum methods
 q1 = Calculator()
